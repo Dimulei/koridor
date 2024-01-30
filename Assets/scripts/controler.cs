afaceab002c5bd9e8f3914667dfcd8fee992e8fb
Dimulei <dkrasavin36@gmail.com>
2024-01-30 08:46:47 +0300

    add jump and obstacles

--- Assets/scripts/controler.cs
+++ Assets/scripts/controler.cs
@@ -7,10 +7,12 @@ public class controler : MonoBehaviour
     public float verta;
     public float horizon;
     public int speed;
+    private Rigidbody rc;
+    public float space;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rc = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
@@ -20,6 +22,10 @@ public class controler : MonoBehaviour
         horizon= Input.GetAxis("Horizontal");
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verta);
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizon);
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rc.AddForce(Vector3.up * space, ForceMode.Impulse);
+        }
 
     }
     private void OnCollisionEnter(Collision collision)
@@ -27,7 +33,17 @@ public class controler : MonoBehaviour
         
         if (collision.gameObject.CompareTag("up"))
         {
+            transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
             Destroy(collision.gameObject);
         }
+        if (collision.gameObject.CompareTag("money"))
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("stope"))
+        {
+            transform.localScale += new Vector3(-0.5f, -0.5f, -0.5f);
+            Destroy(collision.gameObject);
+        }   
     }
 }
